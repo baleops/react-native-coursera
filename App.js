19e8889dde1983c8922eca5371d3bf8e9bcd5a21
baleops <muhammad.iqbal14115014@gmail.com>
2020-09-29 16:34:54 +0700

    Component 1 dan Component 2

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My app</Text>
-      <Text>My app</Text>
-      <Text>My app</Text>
-      <Text>My app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main/>
   );
 }
 
